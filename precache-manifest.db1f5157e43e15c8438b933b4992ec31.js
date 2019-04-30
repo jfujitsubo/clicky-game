d81a80e87c2167e961592b26412a0a4719b440ba
Fooji <jfujitsubo@yahoo.com>
2019-04-29 19:16:57 -0700

    Updates

---
+++ precache-manifest.db1f5157e43e15c8438b933b4992ec31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4c3f31b9ec01dbaa05bea495acbdfd",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "88e23b9ace2ca5b8c117",
+    "url": "/clicky-game/static/css/main.fafe2f73.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "88e23b9ace2ca5b8c117",
+    "url": "/clicky-game/static/js/main.1f2f9026.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
